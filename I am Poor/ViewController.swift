08635240bc2c75ceb4098f5eeaf31c6f1326f4e1
Dan Sehl <dan.sehl@gmail.com>
2020-12-28 10:13:24 -0700

    Initial Commit

---
+++ I am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Poor
+//
+//  Created by Dan Sehl on 12/28/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
